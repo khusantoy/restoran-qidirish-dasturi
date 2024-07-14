e17acbc0783e2da7070e68a269f13727efff0033
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-07-15 02:10:45 +0500

    updated getting curreny location

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,9 +1,5 @@
-import 'dart:math';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:restoran_qidirish_dasturi/controllers/auth_controller.dart';
 import 'package:restoran_qidirish_dasturi/services/geolocator_service.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
@@ -15,27 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Point? myLocation = const Point(latitude: 40.7128, longitude: -74.0060);
+  Point? myLocation;
   late YandexMapController mapController;
-
-  @override
-  void initState() {
-    super.initState();
-    GeolocatorService.getLocation().then(
-      (value) {
-        myLocation =
-            Point(latitude: value.latitude, longitude: value.longitude);
-        mapController.moveCamera(
-          animation: const MapAnimation(),
-          CameraUpdate.newCameraPosition(
-            CameraPosition(
-              target: myLocation!,
-            ),
-          ),
-        );
-      },
-    );
-  }
 
   void goToMyLocation() {
     mapController.moveCamera(
@@ -43,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
       CameraUpdate.newCameraPosition(
         CameraPosition(
           target: myLocation!,
+          zoom: 20,
         ),
       ),
     );
@@ -50,80 +28,91 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void onMapCreated(YandexMapController controller) {
     mapController = controller;
+    goToMyLocation();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          YandexMap(
-            onMapCreated: onMapCreated,
-            mapObjects: [
-              PlacemarkMapObject(
-                mapId: const MapObjectId("myLocation"),
-                point: myLocation!,
-                icon: PlacemarkIcon.single()
-              )
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: MediaQuery.of(context).viewPadding.top + 20,
-            ),
-            child: const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
+      body: FutureBuilder<Position>(
+        future: GeolocatorService.getLocation(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            myLocation = Point(
+              latitude: snapshot.data!.latitude,
+              longitude: snapshot.data!.longitude,
+            );
+
+            return Stack(
+              children: [
+                YandexMap(
+                  onMapCreated: onMapCreated,
+                  mapObjects: [
+                    PlacemarkMapObject(
+                      mapId: const MapObjectId("myLocation"),
+                      point: myLocation!,
+                      icon: PlacemarkIcon.single(
+                        PlacemarkIconStyle(
+                          image: BitmapDescriptor.fromAssetImage(
+                              "assets/route_start.png"),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: MediaQuery.of(context).viewPadding.top + 20,
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search here",
+                    ),
                   ),
                 ),
-                filled: true,
-                fillColor: Colors.white,
-                prefixIcon: Icon(Icons.search),
-                hintText: "Search here",
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 10),
-              child: FloatingActionButton(
-                onPressed: () {
-                  goToMyLocation();
-                },
-                backgroundColor: Colors.white,
-                child: const Icon(Icons.location_searching),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  FloatingActionButton(
-                    onPressed: () {},
-                    backgroundColor: Colors.white,
-                    child: const Icon(Icons.add),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {},
+                          backgroundColor: Colors.white,
+                          child: const Icon(Icons.add),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        FloatingActionButton(
+                          onPressed: () {},
+                          backgroundColor: Colors.white,
+                          child: const Icon(Icons.remove),
+                        )
+                      ],
+                    ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {},
-                    backgroundColor: Colors.white,
-                    child: const Icon(Icons.remove),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
+                )
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
